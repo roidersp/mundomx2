a07a9fa154d424f37d593eab438182e430b254ad
Rodrigo Luis Santander <roidersp@gmail.com>
2015-01-20 10:52:57 -0600

    sdsdf

---
+++ js/indepth.js
@@ -0,0 +1,54 @@
+var w=$(window).width();
+var h=$(window).height();
+var mobile=false;
+
+
+
+$(".indepth_imag_cont").css("width",w+"px");
+$(".indepth_cont").css("height",h+"px");
+
+$(".indeth_portada").css("width",w+"px");
+$(".indeth_portada").css("height",h+"px");
+
+
+var device = navigator.userAgent
+
+if (device.match(/Iphone/i)|| device.match(/Ipod/i)|| device.match(/Android/i)|| device.match(/J2ME/i)|| device.match(/BlackBerry/i)|| device.match(/iPhone|iPad|iPod/i)|| device.match(/Opera Mini/i)|| device.match(/IEMobile/i)|| device.match(/Mobile/i)|| device.match(/Windows Phone/i)|| device.match(/windows mobile/i)|| device.match(/windows ce/i)|| device.match(/webOS/i)|| device.match(/palm/i)|| device.match(/bada/i)|| device.match(/series60/i)|| device.match(/nokia/i)|| device.match(/symbian/i)|| device.match(/HTC/i))
+ { 
+mobile=true;
+
+}
+else
+{
+	mobile=false;
+}
+
+
+if(mobile){
+
+	
+}else{
+
+}
+
+
+
+
+$(window).on("resize", function(){
+	 w=$(window).width();
+	 h=$(window).height();
+
+ 
+ if(mobile){
+
+	
+	
+}else{
+	
+}
+ 
+ 
+});
+
+
+
